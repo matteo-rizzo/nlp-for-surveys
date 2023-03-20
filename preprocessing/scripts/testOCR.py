673f57291d46a10e9fa667d2e88b5384e77ab05a
Matteo Marcuzzo <matteo.marcuzzo.it@gmail.com>
2023-03-20 14:48:25 +0100

    test 2

--- preprocessing/scripts/testOCR.py
+++ preprocessing/scripts/testOCR.py
@@ -1,11 +1,21 @@
 import json
 import os
 from pathlib import Path
-import ocrmypdf
+import layoutparser as lp
+import cv2
+import numpy as np
+from pdf2image import pdf2image
+
 
 def main():
     path = "data/papers/8320/Mcgrath and Heiens - 2003 - Beware the Internet panacea How tried and true st.pdf"
-    ocrmypdf.ocr(path, 'output.pdf', deskew=True)
+    img = np.asarray(pdf2image.convert_from_path(path)[0])
+
+    model = lp.Detectron2LayoutModel('lp://PubLayNet/mask_rcnn_X_101_32x8d_FPN_3x/config',
+                                     extra_config=["MODEL.ROI_HEADS.SCORE_THRESH_TEST", 0.5],
+                                     label_map={0: "Text", 1: "Title", 2: "List", 3: "Table", 4: "Figure"})
+    layout_result = model.detect(img)
+    lp.draw_box(img, layout_result, box_width=5, box_alpha=0.2, show_element_type=True)
     # folder_path: Path = Path("data/processed/success")
     #
     # success_list: list[str] = os.listdir(folder_path)
