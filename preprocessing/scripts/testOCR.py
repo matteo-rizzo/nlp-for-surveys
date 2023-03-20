32b93afafcf2a123b8ffbb62d2383029725cce9f
Matteo Marcuzzo <matteo.marcuzzo.it@gmail.com>
2023-03-20 15:59:46 +0100

    It's doing something, but I don't know what

--- preprocessing/scripts/testOCR.py
+++ preprocessing/scripts/testOCR.py
@@ -1,21 +1,15 @@
-import json
-import os
-from pathlib import Path
-import layoutparser as lp
 import cv2
+import layoutparser as lp
 import numpy as np
 from pdf2image import pdf2image
 
-
 def main():
     path = "data/papers/8320/Mcgrath and Heiens - 2003 - Beware the Internet panacea How tried and true st.pdf"
-    img = np.asarray(pdf2image.convert_from_path(path)[0])
-
-    model = lp.Detectron2LayoutModel('lp://PubLayNet/mask_rcnn_X_101_32x8d_FPN_3x/config',
-                                     extra_config=["MODEL.ROI_HEADS.SCORE_THRESH_TEST", 0.5],
-                                     label_map={0: "Text", 1: "Title", 2: "List", 3: "Table", 4: "Figure"})
-    layout_result = model.detect(img)
-    lp.draw_box(img, layout_result, box_width=5, box_alpha=0.2, show_element_type=True)
+    pdf_layout = lp.load_pdf(path)
+    # print(pdf_layout[0])  # the layout for page 0
+    pdf_layout, pdf_images = lp.load_pdf(path, load_images=True)
+    img = lp.draw_box(pdf_images[1], pdf_layout[1])
+    img.show()
     # folder_path: Path = Path("data/processed/success")
     #
     # success_list: list[str] = os.listdir(folder_path)
