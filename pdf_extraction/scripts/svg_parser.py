ad8410bf45cda42dbddbeddb78849a53f2af7058
Matteo Marcuzzo <matteo.marcuzzo.it@gmail.com>
2023-03-23 16:02:49 +0100

    Updated tree

--- pdf_extraction/scripts/svg_parser.py
+++ pdf_extraction/scripts/svg_parser.py
@@ -1,4 +1,22 @@
+import os
 import xml.etree.ElementTree as ET
+from pathlib import Path
+
+
+class SVGTree:
+    def __init__(self, root: ET.Element):
+        self.root: ET.Element = root
+        self.current_index: int = 0
+        self.nodes: dict[int, list[str]] = {}
+
+    def add_node(self):
+        index = self.current_index
+        self.current_index += 1
+        self.nodes[index] = []
+        return index
+
+    def add_to_node(self, index: int, text: str):
+        self.nodes[index].append(text)
 
 
 def get_tag(child):
@@ -10,34 +28,50 @@ def get_tag(child):
         return child.tag
 
 
-def traverse(node):
+# I want to keep the tree information, though
+def traverse(node: ET.Element, tree: SVGTree):
     # Check if the node has children
-    children = node.findall('*')
+    children: list[ET.Element] = node.findall('*')
     if children:
-        # Node has children, so print its tag name and attributes
-        tag = get_tag(node)
-        if tag == "text":
-            print(tag, node.attrib)
+        # Node has children, keep going
         # Recursively traverse the children
-        for child in children:
-            traverse(child)
+        tag: str = get_tag(node)
+        if tag == "text":
+            index = tree.add_node()
+            for child in children:
+                text_span = traverse(child, tree)
+                tree.add_to_node(index, text_span)
+        else:
+            # Ignore output
+            for child in children:
+                traverse(child, tree)
     else:
         # Node is a leaf node, so print its text of tspan
-        tag = get_tag(node)
+        tag: str = get_tag(node)
+        # The SVG <tspan> element defines a subtext within a <text> element or another <tspan> element.
         if tag == "tspan":
-            print(f"\t[{tag}] {node.text}")
+            # print(f"\t[{tag}] {node.text}")
+            return node.text
+
+
+def read_file(path):
+    svg_tree = ET.parse(path)
+
+    # Get the root element of the SVG tree
+    svg_root: ET.Element = svg_tree.getroot()
+
+    text_tree: SVGTree = SVGTree(svg_root)
+    # Traverse the SVG tree
+    traverse(text_tree.root, text_tree)
 
 
 def main():
     # Load the SVG file
-    svg_file = 'data/papers/8239/svg/Hindawi - 2009 - 2020 A Publishing Odyssey_p0.svg'
-    svg_tree = ET.parse(svg_file)
-
-    # Get the root element of the SVG tree
-    svg_root = svg_tree.getroot()
-
-    # Traverse the SVG tree
-    traverse(svg_root)
+    svg_folder: Path = Path("data/papers/8239/svg")
+    files = os.listdir(svg_folder)
+    for file in files:
+        svg_path: Path = svg_folder / str(file)
+        read_file(svg_path)
 
 
 if __name__ == "__main__":
