fee1926ce3050f9f701944e565b08476baef1171
Matteo Marcuzzo <matteo.marcuzzo.it@gmail.com>
2023-03-30 17:35:41 +0200

    Cleaner

--- pdf_extraction/scripts/approx_section_extraction.py
+++ pdf_extraction/scripts/approx_section_extraction.py
@@ -9,19 +9,30 @@ from pdf_extraction.classes.pdftree import PDFTree
 
 def abstract_and_text_to_json():
     for i in tqdm(range(8239, 9882)):
+        # FIXME: CRASH AT "ORIGINAL_NAME"
         tree = PDFTree(i)
         path = Path("data/papers") / str(i)
         text = extract_text(path / tree.json_info['original_name'])
         data = {
-            "Abstract": tree.json_info["ris"].get("abstract", ""),
+            "Ris": tree.json_info["ris"],
             "Full Text": text
         }
         with open(path / f"content.json", 'w') as f:
             json.dump(data, f, indent=2)
 
 
+# 1 - Find repeated blocks and remove them (done)
+# 2 - Remove numbers (done)
+# 3 - Remove \t and \r
+# 4 - Remove strings which are just characters divided by spaces (unreadable)
+# 5 - Remove short blocks (but what if they merge?)
+# 6 - Remove strings with weird characters like copyright
+# 7 - Remove references (from bottom)
+# 8 - remove links and emails
+
+# END - when join all together, clean again from garbage: DOIS, links, emails
 def clean_text():
-    # Ok it crashed after a while so only 178 were done but we'll fix that later
+    # FIXME: CRASH AT "ORIGINAL_NAME"
     for i in tqdm(range(8239, 8239 + 178)):
         path = Path("data/papers") / str(i) / "content.json"
         with open(path, "r") as bf:
@@ -31,14 +42,31 @@ def clean_text():
 
         current_block = []
         block_text = []
+        # --- merge blocks ----
         for line in line_text:
             if line == "":
                 block = " ".join(current_block)
                 block_text.append(block)
-                block_text.append("")
+                block_text.append("[BLK]")
                 current_block = []
             else:
                 current_block.append(line)
+        print("hey")
+        # --- Find blocks which have duplicates (probably header / footers) ---
+        seen = []
+        to_remove: set[int] = set()
+        for index, block in enumerate(block_text):
+            if block in seen:
+                to_remove.add(index)
+                to_remove.add(seen.index(block))
+            seen.append(block)
+        # --- Only keep non duplicate blocks ---
+        non_duplicate_text = []
+        for index, block in enumerate(block_text):
+            if index not in to_remove:
+                # -- Also check it's not just a number (probably page number)
+                if not block.strip().isdigit():
+                    non_duplicate_text.append(block)
         print("hey")
         # print(text)
 
