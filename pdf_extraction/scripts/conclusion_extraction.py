721dd60e47a43c14dea8b63a169103e90ee3c38f
Matteo Marcuzzo <matteo.marcuzzo.it@gmail.com>
2023-05-04 15:21:31 +0200

    Conclusion extraction

---
+++ pdf_extraction/scripts/conclusion_extraction.py
@@ -0,0 +1,53 @@
+import os
+from pathlib import Path
+from typing import Optional
+
+import ujson
+from tqdm import tqdm
+
+
+def extract_conclusions(data: dict) -> Optional[str]:
+    full_text: str = data["Full Text"]
+
+    for i in range(len(full_text) - 1, -1, -1):
+        if "conclusion" in full_text[i].lower():
+            extracted_conclusion: str = " ".join(full_text[i:])
+            # TODO: check length
+            return extracted_conclusion
+    return ""
+
+
+def main():
+    prefix = "data/papers"
+    out = Path("data/with_conclusion")
+    out.mkdir(exist_ok=True)
+    paper_paths: list[Path] = [Path(f"{prefix}/{x}/clean_content.json") for x in os.listdir(prefix)]
+    skipped: int = 0
+    for content in tqdm(paper_paths, desc="Adding conclusions"):
+        if not content.exists():
+            skipped += 1
+            continue
+        with open(content, "r", encoding="UTF-8") as f:
+            data = ujson.load(f)
+        # This seemed like a good idea, but it's not :(
+        #     path_to_pdf = [f for f in os.listdir(content.parent) if f.endswith('.pdf')][0]
+        #     pdf_file: PdfDocument = PdfDocument(content.parent / path_to_pdf)
+        #     toc = []
+        #     # This finds TOC if embedded in pdf already.
+        #     for item in pdf_file.get_toc():
+        #         if item.n_kids == 0:
+        #             state = "*"
+        #         elif item.is_closed:
+        #             state = "-"
+        #         else:
+        #             state = "+"
+        #         toc.append((state, item.title))
+        # data["toc"] = toc
+        data["conclusions"] = extract_conclusions(data)
+        with open(out / f"{content.parent.name}.json", "w", encoding="UTF-8") as f:
+            ujson.dump(data, f, indent=2)
+    print(f"Skipped {skipped} files (could not extract text).")
+
+
+if __name__ == "__main__":
+    main()
