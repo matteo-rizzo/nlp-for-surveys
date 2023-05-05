69795047a6b8fd8c2c1a9ffa1008a25f50833f31
alek9z <alessandro.zangari.code@outlook.com>
2023-05-05 12:01:34 +0200

    Add hyperparameter tuning facilities for KMeans and HDBSCAN

--- topic_extraction/classes/BERTopicExtractor.py
+++ topic_extraction/classes/BERTopicExtractor.py
@@ -1,13 +1,11 @@
 from __future__ import annotations
 
 import copy
-import datetime
+import itertools
 import logging
 from pathlib import Path
-from pprint import pprint
+from typing import Callable, Generic, TypeVar, Iterable
 
-from sklearn.cluster import KMeans
-from topictuner import TopicModelTuner as TMT
 import matplotlib.pyplot as plt
 import networkx as nx
 import numpy as np
@@ -17,19 +15,63 @@ from bertopic.representation import MaximalMarginalRelevance, KeyBERTInspired
 from bertopic.vectorizers import ClassTfidfTransformer
 from hdbscan import HDBSCAN
 from sentence_transformers import SentenceTransformer
+from sklearn.cluster import KMeans
 from sklearn.decomposition import PCA
 from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics import silhouette_score
+from sklearn.metrics.pairwise import cosine_similarity
+from topictuner import TopicModelTuner as TMT
 from umap import UMAP
 
 from topic_extraction.classes import Document
 from topic_extraction.classes.BERTopicExtended import BERTopicExtended
 from topic_extraction.classes.BaseTopicExtractor import BaseTopicExtractor
 from topic_extraction.utils import load_yaml
-from sklearn.metrics.pairwise import cosine_similarity
-
 from topic_extraction.visualization.plotly_graph import plot_network
 from topic_extraction.visualization.utils import visualize_topic_space_data
 from topic_extraction.visualization.visualize_stacked_topics import visualize_stacked_topics
+
+T = TypeVar("T")
+
+
+def grid_search(estimator_class: T, grid_params: dict, metric_fun: Callable[[Generic[T]], float], large_is_better: bool,
+                estimator_fit_args: Iterable = None, estimator_fit_kwargs: dict = None, estimator_kwargs: dict = None) -> list[dict]:
+    print("Started grid search...")
+
+    estimator_fit_args = estimator_fit_args if estimator_fit_args is not None else list()
+    estimator_fit_kwargs = estimator_fit_kwargs if estimator_fit_kwargs is not None else dict()
+    estimator_kwargs = estimator_kwargs if estimator_kwargs is not None else dict()
+
+    best_score = -1.0
+    best_parameters = None
+    all_results = list()
+
+    compare_fn = float.__gt__ if large_is_better else float.__lt__
+
+    k_args, args_list = zip(*grid_params.items())
+    args_len = [list(range(len(a))) for a in args_list]
+    args_index_combinations: list[tuple] = itertools.product(*args_len)
+
+    for comb_idx in args_index_combinations:
+        # Prepare argument combination
+        vals = [arg[i] for arg, i in zip(args_list, comb_idx)]
+        kv_args = dict(zip(k_args, vals))
+
+        # Clustering
+        est = estimator_class(**kv_args, **estimator_kwargs).fit(*estimator_fit_args, **estimator_fit_kwargs)
+        # DBCV score
+        score = metric_fun(est)
+        n_clusters = int(est.labels_.max() + 1)
+        ext_args = {**kv_args, "score": score, "n_clusters": n_clusters}
+        # if we got a better score, store it and the parameters
+        if compare_fn(score, best_score):
+            best_score = score
+            best_parameters = ext_args
+        all_results.append(ext_args)
+
+    print("Best score: {:.4f}".format(best_score))
+    print("Best parameters: {}".format(best_parameters))
+    return all_results
 
 
 class BERTopicExtractor(BaseTopicExtractor):
@@ -57,6 +99,68 @@ class BERTopicExtractor(BaseTopicExtractor):
     @staticmethod
     def tl_factory(tl_args: dict) -> BERTopic:
         return BERTopicExtended(**tl_args)
+
+    def find_optimal_n_clusters(self, documents, conf_search: str | Path, **kwargs) -> None:
+
+        conf_search = load_yaml(conf_search)["clustering"]
+
+        documents = [d.body for d in documents]
+
+        embeddings = self._embedding_model.encode(documents, show_progress_bar=False)
+
+        if kwargs.get("normalize", False):
+            embeddings /= np.linalg.norm(embeddings, axis=1).reshape(-1, 1)
+
+        umap_embeddings = self._topic_model._reduce_dimensionality(embeddings)
+
+        # 2. Select best hyperparameters
+
+        if isinstance(self._clustering_model, KMeans):
+            c = conf_search["kmeans"]
+            distortions, silhouette_scores = [], []
+            k_range = range(c["k_start"], c["k_end"])
+            for k in k_range:
+                clustering = KMeans(n_clusters=k, n_init="auto", random_state=0)
+                clustering.fit(umap_embeddings)
+                distortions.append(clustering.inertia_)  # lower the better
+                silhouette_scores.append(silhouette_score(umap_embeddings, clustering.labels_))  # higher the better (ideal > .5)
+
+            fig, ax = plt.subplots(1, 2, figsize=(12, 4))
+            ax[0].plot(k_range, distortions, "bx-")
+            ax[0].set_xlabel("Number of Clusters")
+            ax[0].set_ylabel("Distortion (the lower the better)")
+            ax[0].set_title("Elbow Method")
+            ax[0].grid(True)
+
+            ax[1].plot(k_range, silhouette_scores, "bx-")
+            ax[1].set_xlabel("Number of Clusters")
+            ax[1].set_ylabel("Silhouette Score (the higher the better)")
+            ax[1].set_title("Silhouette Method")
+            ax[1].grid(True)
+
+            plt.show()
+
+            optimal_n_clusters = np.argmax(silhouette_scores) + c["k_start"]
+        else:
+            # logging.captureWarnings(True)
+            # parameters and distributions to sample from
+            c = conf_search["hdbscan"]
+
+            # Define the score function
+            def fun_dbcv(est: HDBSCAN) -> float:
+                return float(est.relative_validity_)
+
+            results = grid_search(HDBSCAN, grid_params=c, metric_fun=fun_dbcv, estimator_fit_args=(umap_embeddings,), large_is_better=True,
+                                  estimator_kwargs=dict(gen_min_span_tree=True))
+
+            dfr = pd.DataFrame.from_records(results).sort_values(by="score", ascending=False)
+
+            self._plot_path.mkdir(exist_ok=True, parents=True)
+            dfr.to_csv(self._plot_path / "hdbscan_grid_results.csv")
+
+            optimal_n_clusters = dfr["n_clusters"][0]
+
+        print(f"The optimal number of clusters is {optimal_n_clusters}")
 
     def prepare(self, *args, **kwargs):
         config_path: str | Path = kwargs.pop("config_file")
---
+++ topic_extraction/tuning.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+
+import torch.cuda
+
+from topic_extraction.classes.BERTopicExtractor import BERTopicExtractor
+from topic_extraction.extraction import document_extraction
+
+# K-MEANS best (specter): 15
+# HDBSCAN best (specter): 'min_samples': 10, 'min_cluster_size': 20, 'metric': 'euclidean', 'cluster_selection_method': 'leaf'
+
+docs = document_extraction()
+
+pl_path1 = Path("plots") / "validation"
+ex1 = BERTopicExtractor(plot_path=pl_path1)
+ex1.prepare(config_file="topic_extraction/config/bertopic2.yml")
+ex1.find_optimal_n_clusters(docs, conf_search="topic_extraction/config/model_selection.yml", normalize=True)
+
+torch.cuda.empty_cache()
+del ex1
